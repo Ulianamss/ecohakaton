d1d70955706f6ffa086fa6bffd0d35e7f678b3b7
Uliana <85169486+Ulianamss@users.noreply.github.com>
2023-02-09 20:34:15 +0300

    Add files via upload

---
+++ src/hooks/route-http.js
@@ -0,0 +1,10 @@
+//TODO: implement http hook for each models in separate files
+
+export const getRoutes = () => {
+    return null
+}
+
+
+export const getRouteById = (name) => {
+    return null;
+}
